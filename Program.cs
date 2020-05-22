754adb2d02772474873e5f4dd947a75ff10780d2
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-22 01:08:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Olá usuário, digite um valor em metros e receba o valor equivalente em quilômetros e centímetros.");
+
+            int metros = Int32.Parse (Console.ReadLine());
+
+
+
+            int centimetros = 100; 
+
+            double quilometros = 0.001;
+
+
+
+            Console.WriteLine (centimetros * metros +  " cm");
+
+            Console.WriteLine (metros +  " m");
+
+            Console.WriteLine (quilometros * metros +  " km");
+        }
+    }
+}
